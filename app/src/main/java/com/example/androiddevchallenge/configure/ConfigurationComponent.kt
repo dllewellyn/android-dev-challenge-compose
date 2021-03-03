b1d69760be4b1dc4cf4e004b64913b24c2525ca7
Daniel Llewellyn <daniel.llewellyn@xdesign.com>
2021-03-03 19:05:19 +0000

    Applied fixe

--- app/src/main/java/com/example/androiddevchallenge/configure/ConfigurationComponent.kt
+++ app/src/main/java/com/example/androiddevchallenge/configure/ConfigurationComponent.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.configure
 
 import com.example.androiddevchallenge.models.Configuration
@@ -5,7 +20,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-
 
 class ConfigurationComponent(
     private val coroutineScope: CoroutineScope,
@@ -21,7 +35,6 @@ class ConfigurationComponent(
 
     fun onSelectedStart() = selectedStart(state.value.toConfiguration())
 }
-
 
 fun ConfigurationComponentState.toConfiguration() =
     Configuration(countdownTimeInSeconds = selectedTime.toInt())
--- app/src/main/java/com/example/androiddevchallenge/timer/TimerScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/timer/TimerScreen.kt
@@ -1,19 +1,41 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.timer
 
-import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.dps
 import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.common.PrimaryButton
 import com.example.androiddevchallenge.models.Configuration
@@ -24,11 +46,13 @@ import kotlinx.coroutines.GlobalScope
 fun TimerComponent.Render() {
     val state = state.collectAsState()
 
-    Scaffold(topBar = {
-        TopAppBar(
-            title = { Text("Timer") },
-        )
-    }) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Timer") },
+            )
+        }
+    ) {
         Column {
             Surface(color = MaterialTheme.colors.background) {
                 when (state.value.timerState) {
